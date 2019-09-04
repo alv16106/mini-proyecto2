dbe811091ab73b5587bea49da2ccfbf1e8413077
Rodrigo Alvarado <alv16106@uvg.edu.gt>
2019-09-04 16:02:08 -0600

    finish 3 y 4 graphs

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,5 +1,6 @@
 import random
 import numpy as np
+import matplotlib.pyplot as plt
 from get_distributions3 import get_first_dist, get_second_dist
 
 
@@ -34,14 +35,54 @@ def get_fluxes(distributions):
     return fluxes
 
 
-def simulate(distributions, k, initial,  n=10000):
+def simulate(distributions, k,  n=10000):
     # recieves list of objects like {'type': 'normal', 'limits': (a,b)}
-    results = [vpn(get_fluxes(distributions), k, initial) for i in range(n)]
+    s = distributions[0]['start']
+    results = [vpn(get_fluxes(distributions[1:]), k, s) for i in range(n)]
     return sum(results) / n
 
 
 if __name__ == "__main__":
+    # Get the 2 distributions
     d1 = get_first_dist()
     d2 = get_second_dist()
-    print(simulate(d1, 0.1, 800))
-    print(simulate(d2, 0.1, 900))
+
+    # SImulate for 100, 1000 and 10000
+    repetitions = [100, 1000, 10000]
+    fp = [simulate(d1, 0.1, r) for r in repetitions]
+    sp = [simulate(d2, 0.1, r) for r in repetitions]
+    print(fp)
+    print(sp)
+
+    # Graphs
+    n_groups = 3
+
+    # create plot
+    fig, ax = plt.subplots()
+    index = np.arange(n_groups)
+    bar_width = 0.35
+    opacity = 0.8
+
+    rects1 = plt.bar(
+        index,
+        fp,
+        bar_width,
+        alpha=opacity,
+        color='c',
+        label='First Project')
+
+    rects2 = plt.bar(
+        index + bar_width,
+        sp,
+        bar_width,
+        alpha=opacity,
+        color='tomato',
+        label='Second Project')
+
+    plt.ylabel('VPN')
+    plt.title('VPN for two projects')
+    plt.xticks(index + bar_width, ('10', '1000', '10000'))
+    plt.legend()
+
+    plt.tight_layout()
+    plt.show()
